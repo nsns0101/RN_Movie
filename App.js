6754e5f03aa5927daf013dac0bd1ba920a686c7d
nsns0101 <nsns0101@naver.com>
2022-03-19 00:42:45 +0900

    movie - AppLoading(2)

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
 import AppLoading from "expo-app-loading";
-import { Text } from "react-native";
+import * as Font from "expo-font";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App(){
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async() => {
     // await new Promise( (resolve) => setTimeout(resolve, 10000) );
-  }
+
+    await Font.loadAsync(Ionicons.font);  //Ionicons에 접근 가능 코드
+    await Asset.loadAsync(require("./dog.jpg"));  //로컬 파일 불러오기
+
+    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+  };
 
   if(!ready){
     return <AppLoading
