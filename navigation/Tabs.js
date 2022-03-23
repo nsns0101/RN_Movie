9327316ac5d841366f994c4b1cddb4442c06ebcc
nsns0101 <nsns0101@naver.com>
2022-03-23 23:41:46 +0900

    movie - Tab Navigation(2)

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
@@ -7,10 +8,22 @@ import Search from "../screens/Search";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-<Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} options={{headerTitleAlign: "center", title:"Movies"}}/>
-    <Tab.Screen name="Tv" component={Tv} options={{headerTitleAlign: "center", title:"Tv"}}/>
-    <Tab.Screen name="Search" component={Search} options={{headerTitleAlign: "center", title:"Search"}}/>
+<Tab.Navigator screenOptions={{
+    headerTitleAlign: "center", 
+    tabBarStyle: { backgroundColor: "tomato"},
+    tabBarActiveTintColor: "red",
+    tabBarInactiveTintColor: "purple",
+    headerTitleStyle: { color: "tomato" },
+    headerRight: () => {
+        return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+        )}
+}}>
+    <Tab.Screen name="Movies" component={Movies} options={{title:"Movies"}}/>
+    <Tab.Screen name="Tv" component={Tv} options={{title:"Tv"}}/>
+    <Tab.Screen name="Search" component={Search} options={{title:"Search"}}/>
 </Tab.Navigator>
 );
 
