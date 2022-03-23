7969526399314e3b0a6756da5ea33d0cfb6c7731
nsns0101 <nsns0101@naver.com>
2022-03-23 20:52:55 +0900

    movie - AppLoading(3)

--- App.js
+++ App.js
@@ -5,16 +5,44 @@ import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+//font배열을 매개인자로 받아서 각각 로드
+// await Font.loadAsync(Ionicons.font);
+// await Font.loadAsync(Ionicons.font);
+// await Font.loadAsync(Ionicons.font);
+// 위처럼 반복되는 것을 방지
+const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
+
+const loadAssets = (images) => images.map(image =>{
+  if(typeof image === "string"){    //형태 ex) "https://~~"
+    return Image.prefetch(image);
+  }
+  else{                             //형태 ex) require("~~~");
+    return Asset.loadAsync(image);
+  }
+})
+
 export default function App(){
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async() => {
     // await new Promise( (resolve) => setTimeout(resolve, 10000) );
 
-    await Font.loadAsync(Ionicons.font);  //Ionicons에 접근 가능 코드
-    await Asset.loadAsync(require("./dog.jpg"));  //로컬 파일 불러오기
+    // await Font.loadAsync(Ionicons.font);  //Ionicons에 접근 가능 코드(loadFonts함수로 대체)
+    const fonts = loadFonts([Ionicons.font]);
+    // console.log(fonts);
+    // console.log("----------------");
+    // console.log([Ionicons.font]);
 
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    // await Asset.loadAsync(require("./dog.jpg"));  //로컬 파일 불러오기(loadAssets함수로 대체)
+    // await Image.prefetch("https://reactnative.dev/img/oss_logo.png");//인터넷 파일 불러오기 문자열이라 prefetch (loadAssets함수로 대체)
+    const images = loadAssets([
+      require("./dog.jpg"), 
+      "https://reactnative.dev/img/oss_logo.png"
+      ]);
+    //  console.log(images);
+    
+    //모든 것에 대한 await 해주기
+    await Promise.all([...fonts, ...images]);
   };
 
   if(!ready){
