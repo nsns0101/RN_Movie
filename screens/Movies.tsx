1cefdc7c01691b6cddcd7a919a71f44b5f647e4f
nsns0101 <nsns0101@naver.com>
2022-03-28 00:07:35 +0900

    Movies Screen - 1

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,27 +1,42 @@
 import React from "react";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+// swiper참고문서 
+//https://github.com/reactrondev/react-native-web-swiper
+import Swiper from 'react-native-web-swiper';
+// Dimensions - 현재 화면의 크기를 알 수 있음
+import { Dimensions } from "react-native";
 
+const API_KEY = "b5ff37f3b8cb4ef5dc5c2bbda9d2c6e8";
 
-console.log()
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
-`;
+`
+const View = styled.View`
+  flex: 1;
+`
+// 현재 화면의 높이 GET
+// == const SCREEN_HEIGHT = Dimensions.get("window").height;
+const {height : SCREEN_HEIGHT} = Dimensions.get("window");
 
-const Title = styled.Text`
-  color: ${(props) => (props.theme.textColor)};
-`;
 
 //??
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-
-);
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {  
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+    )
+  }
+  return (
+    <Container>
+      {/*  */}
+      <Swiper  loop timeout={3.5} containerStyle={{ wdith: "100%", height: SCREEN_HEIGHT / 4}}>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  )
+};
 export default Movies;
