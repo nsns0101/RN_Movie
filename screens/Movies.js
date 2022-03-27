719f5ef38b4a235833aac1a7621ff5b688b1a9e5
nsns0101 <nsns0101@naver.com>
2022-03-27 14:44:20 +0900

    styled Components

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,13 +1,22 @@
 import React from "react";
 import {View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-    <TouchableOpacity
-      onPress={() => navigate("Stack", { screen: "Three" })}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Movies</Text>
-    </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
   );
 
 export default Movies;
